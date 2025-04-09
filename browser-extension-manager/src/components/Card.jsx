5b9a5d32905d9622496f8f2a40210c20cfbc3fd3
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-09 15:02:40 +0500

    updated true false to card

--- browser-extension-manager/src/components/Card.jsx
+++ browser-extension-manager/src/components/Card.jsx
@@ -4,15 +4,16 @@ import Button from "./Button"
 const Card = ({logo, name, description, isActive}) => {
   const [isOn, setIsOn] = useState(false)
 
-  function handleClick(e){
-    setIsOn(!isOn)
+  function handleClick(){
+    isActive ? isActive = false : isActive = 
+    console.log(isActive)
     
   }
   function removeBlock(e){
     
   }
   return (
-    <div id={isActive} className="bg-gray-800 h-44 bg-opacity-10 rounded-2xl p-4 border-[1px] border-gray-50 border-opacity-40 relative">
+    <div id={isActive.toString()} className="bg-gray-800 h-44 bg-opacity-10 rounded-2xl p-4 border-[1px] border-gray-50 border-opacity-40 relative">
       <div className="flex gap-3 mb-7">
         <img src={logo} className="w-12 h-12" alt="logo" />
         <div>
@@ -22,8 +23,8 @@ const Card = ({logo, name, description, isActive}) => {
       </div>
       <div className="absolute bottom-4 flex justify-between items-center w-[87%]">
         <button onClick={(e) => {removeBlock(e)}} className="border-[1px] border-gray-50 text-white rounded-2xl px-4 py-1 text-center cursor-pointer hover:bg-red-500 hover:text-black hover:border-black">Remove</button>
-        <div onClick={handleClick} className={`relative rounded-full w-9 h-4 cursor-pointer  ${isOn ? 'bg-red-400' : 'bg-gray-400'}`}>
-          <div className={`absolute w-4 h-4 bg-white rounded-full  ${isOn ? ' left-5' : ' left-0'}`}>
+        <div onClick={handleClick} className={`relative rounded-full w-9 h-4 cursor-pointer  ${isActive ? 'bg-red-400' : 'bg-gray-400'}`}>
+          <div className={`absolute w-4 h-4 bg-white rounded-full  ${isActive ? ' left-5' : ' left-0'}`}>
           </div>
         </div>
       </div>
